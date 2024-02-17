be9082bf0c669b0f0770068555af5f7eec6dc971
Atharva Meshram <atharva.g.meshram@gmail.com>
2024-02-17 21:43:53 +0530

    initial commit

---
+++ frontend/src/Dashboard/doctor-account/prescriptionGenerator/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <footer className='footer border-t-2 border-gray-200 pt-5'>
+            <ul className="flex flex-wrap items-center justify-center">
+              <li className='px-2'>Medicare </li>
+              <li className='px-2'>medicareconnect@gmail.com </li>
+              <li className='px-4'>https://medicare.com</li>
+
+            </ul>
+          </footer>
+    </>
+  )
+}
+
+export default Footer
